6aa8741855608aa2ea76621e546d0f822b56312d
Adikesh59 <adikesh59@gmail.com>
2025-11-03 15:00:12 +0530

    Updated backend code for Vercel deployment

--- server.js
+++ server.js
@@ -10,42 +10,25 @@ app.use(express.json())
 //This line tells your Express app to automatically read JSON data that comes in requests.
 
 //Using Cors to connect backend to frontend
-// app.use(cors({
-//   origin:true,
-//   methods:["GET","POST","PUT","DELETE"],
-//   credentials:true
-// }))
-
-//Vercel change start
-// ✅ Correct CORS setup (frontend URL fix)
 app.use(cors({
-  origin: [
-    "http://localhost:5173", // for local dev
-    "https://recipe-app-frontend-mern.vercel.app" // ✅ your deployed frontend
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
-// ✅ Default route (for testing backend connection)
-// app.get("/", (req, res) => {
-//   res.send("Backend is live and connected successfully!");
-// });
-//Vercel change end
+  origin:true,
+  methods:["GET","POST","PUT","DELETE"],
+  credentials:true
+}))
 
 //userRouter
-// app.use('/backend', userRouter)
+app.use('/backend/user', userRouter)
 /*Tells the app to use all the routes from userRouter,
 and make them available under the /backend path. */
 
 //recipeRouter
-// app.use('/backend', recipeRouter)
+app.use('/backend/recipe', recipeRouter)
 
-//Vercel change
-app.use("/backend/user", userRouter);
-app.use("/backend/recipe", recipeRouter);
-//end
 
-mongoose.connect("mongodb+srv://root:root@cluster0.radrflo.mongodb.net/", {
+// mongoose.connect("mongodb+srv://root:root@cluster0.radrflo.mongodb.net/", {
+//   dbName: "Recipe_App_MERN",
+// })
+mongoose.connect(process.env.MONGO_URI, {
   dbName: "Recipe_App_MERN",
 })
   .then(() => {
@@ -56,7 +39,10 @@ mongoose.connect("mongodb+srv://root:root@cluster0.radrflo.mongodb.net/", {
   });
 
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`The server is started at http://localhost:${PORT}`);
-});
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`The server is started at http://localhost:${PORT}`);
+// });
+
+// For vercel
+export default app;
